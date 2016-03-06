c067799ded20456c142735edbecfab60ef5fbccf
dac482 <diegocarrillo482@gmail.com>
2016-03-05 22:14:29 -0600

    Funcionalidad comentarios-DB
    Comentarios index funciona con Data Base

--- contac.php
+++ contac.php
@@ -12,8 +12,8 @@ $msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Mensaje:\n $mensaje";
 
 
 /*Conexión y consulta para insertar en la base de datos.*/
-$conexion = mysql_connect("www.bravoutd.com" , "betobrav_consult" , "consul9040");
-mysql_select_db("betobrav_consultic",$conexion);
+$conexion = mysql_connect("localhost" , "" , "");
+mysql_select_db("nanny",$conexion);
 $sql = "INSERT INTO contacto (nombre, email, mensaje) VALUES ('$nombre','$email','$mensaje')";
 mysql_query($sql);
 
@@ -25,7 +25,7 @@ if ($_POST['submit'])
 if (mail($para, $titulo, $msjCorreo, $header)) {
 echo "<script language='javascript'>
 alert('Mensaje enviado, responderemos a la brevedad...Gracias!.');
-window.location.href = 'http://www.consultic.bravoutd.com/';
+window.location.href = 'http://http://localhost/EstadiaQueretaro/index.html';
 </script>";
 } else {
 echo 'Falló el envio, intenta de nuevo porfavor.';
