b554e7241ca464b67b6a7b9a22a90a24be36d800
saidwn <saidvara@gmail.com>
2016-03-22 22:33:23 -0600

    cambio de manera de iniciar sesion

---
+++ iniciar_como.php
@@ -0,0 +1,85 @@
+
+<!--INICIO DEL CONTENIDO-->
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Niñeras</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<link href='http://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.13.1/jquery.validate.min.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+	</head>
+	<body class="homepage">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<div id="header-wrapper">
+					<div id="header" class="container">
+
+						<!-- Logo -->
+							
+
+						<!-- Nav -->
+							<nav id="nav">
+								<ul>
+									<li><a href="index.html"><i class="fa fa-home fa-2x"></i><span> INICIO</span></a></li>
+									<li><a href="#"><i class="fa fa-users fa-2x"></i> </i><span> FAMILIA</span></a></li>
+									<li><a href="#"><i class="fa fa-female fa-2x"></i> </i><span> NIÑERA</span></a></li>
+									<li><a href="acerca.html"><i class="fa fa-info-circle fa-2x"></i> </i><span> ACERCA DE</span></a></li>
+									<li><a href="recomendaciones.html"><i class="fa fa-file-text-o fa-2x"></i><span>  RECOMENDACIONES</span></a></li>
+								</ul>
+								
+							</nav>
+ 
+ 
+					
+
+							<div class="row">
+							<div class="6u 12u(mobile)">
+
+								<!-- Feature -->
+									<section>
+										<a href="login_familias.php" class="image featured"><img src="images/familia.png" alt="" width="500" /></a>
+										<a href="login_familias.php"><input id="submit" type="submit" name="submit" value="Familia" style="width:400px;height:45px"/></a>
+									</section>
+
+							</div>
+							<div class="6u 12u(mobile)">
+
+								<!-- Feature -->
+									<section>
+										<a href="login_nineras.php" class="image featured"><img src="images/ninera.jpg" alt="" width="500" /></a>
+										<a href="login_nineras.php"><input id="submit" type="submit" name="submit" value="Niñera" style="width:400px;height:45px"/></a>
+									</section>
+
+							</div>
+							</div>
+				
+</div>
+				
+   </div>
+
+	</div>	
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/skel-viewport.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
